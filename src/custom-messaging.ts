5f79fdcd6c0bafba6b3590f5bcba701c8492b679
xxxhand <nohandboy@gmail.com>
2024-03-20 19:33:57 +0800

    move fs-extra to dependency

--- src/custom-messaging.ts
+++ src/custom-messaging.ts
@@ -1,16 +1,21 @@
+import * as fs from 'fs-extra';
 import firebase from 'firebase-admin';
 import { App, initializeApp, ServiceAccount } from 'firebase-admin/app';
 import { Message, MulticastMessage } from 'firebase-admin/messaging';
 
-export class CustomFcmClient {
+export class CustomMessaging {
 
   private _app: App;
   constructor(absoluteCredentialPath: string) {
-    const serviceAccount = require(absoluteCredentialPath) as ServiceAccount;
+    const serviceAccount = this._readCredentialAsServiceAccount(absoluteCredentialPath);
     this._app = initializeApp({ credential: firebase.credential.cert(serviceAccount)});
   }
 
-  private _readCredentialAsServiceAccount(): ServiceAccount {
-    
+  private _readCredentialAsServiceAccount(credentialPath: string): ServiceAccount {
+    try {
+      const buf = 
+    } catch (error) {
+      
+    }
   }
 }
