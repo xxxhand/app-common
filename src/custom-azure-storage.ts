b43fc30685c45cdf09a55af6f72f63bc03faacbf
WilbertYang <Wilbert_Yang@compal.com>
2024-04-12 00:58:29 +0800

    Add custom azure storage

---
+++ src/custom-azure-storage.ts
@@ -0,0 +1,100 @@
+import { BlobServiceClient, ContainerClient, SASProtocol } from '@azure/storage-blob';
+
+export class CustomAzureStorage {
+  // connection string
+  private _connectionString: string = '';
+
+  // container name
+  private _containerName: string = '';
+
+  // blob service client
+  private _blobServiceClient: BlobServiceClient | null = null;
+
+  private readonly _error_prefix = '[custom-azure-storage]';
+
+  // Set connection string
+  public useConnectionString(connectionString: string): CustomAzureStorage {
+    this._connectionString = connectionString;
+    return this;
+  }
+
+  // Set container name
+  public useContainerName(containerName: string): CustomAzureStorage {
+    this._containerName = containerName;
+    return this;
+  }
+
+  // Initial azure storage instance
+  public initial(): CustomAzureStorage {
+    this._blobServiceClient = BlobServiceClient.fromConnectionString(this._connectionString);
+    return this;
+  }
+
+  // Get container client
+  public getContainerClient(): ContainerClient {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    return this._blobServiceClient.getContainerClient(this._containerName);
+  }
+
+  // Upload file to azure storage
+  public async uploadFile(sourceFile: string, targetFile: string): Promise<void> {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    const containerClient = this._blobServiceClient.getContainerClient(this._containerName);
+    if (!await containerClient.exists()) await containerClient.create();
+    const blobClient = containerClient.getBlockBlobClient(targetFile);
+    await blobClient.uploadFile(sourceFile);
+  }
+
+  // Download file from azure storage
+  public async downloadFile(fileName: string): Promise<ReadableStream | undefined> {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    const containerClient = this._blobServiceClient.getContainerClient(this._containerName);
+    const blobClient = containerClient.getBlockBlobClient(fileName);
+    if (!blobClient.exists()) throw new Error(`${this._error_prefix} The file is not exist`);
+    const blob =  await blobClient.download();
+    return blob.readableStreamBody as ReadableStream<any> | undefined;
+  }
+
+  // Delete the file from azure storage
+  public async deleteFile(fileName: string): Promise<void> {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    const containerClient = this._blobServiceClient.getContainerClient(this._containerName);
+    const blobClient = containerClient.getBlockBlobClient(fileName);
+    await blobClient.delete();
+  }
+
+  // Copy the file from azure storage
+  public async copyFile(sourceFileName: string, targetFileName: string): Promise<void> {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    const containerClient = this._blobServiceClient.getContainerClient(this._containerName);
+    const sourceBlobClient = containerClient.getBlockBlobClient(sourceFileName);
+    const targetBlobClient = containerClient.getBlockBlobClient(targetFileName);
+    const copyPoller = await targetBlobClient.beginCopyFromURL(sourceBlobClient.url);
+    await copyPoller.pollUntilDone();
+  }
+
+  // Create blob SAS token
+  public async createBlobSasToken(fileName: string, duration: number): Promise<string> {
+    if (!this._blobServiceClient) {
+      throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    }
+    const containerClient = this._blobServiceClient.getContainerClient(this._containerName);
+    const blobClient = containerClient.getBlockBlobClient(fileName);
+    const sasToken = await blobClient.generateSasUrl({
+      protocol: SASProtocol.Https,
+      startsOn: new Date(),
+      expiresOn: new Date(new Date().getTime() + duration),
+    });
+    return sasToken;
+  }
+}
