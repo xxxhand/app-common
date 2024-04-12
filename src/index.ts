2cd4531970a4993e48bbadddc88c0e6f390cc3a9
xxxhand <nohandboy@gmail.com>
2024-04-12 15:36:43 +0800

    export mongo and azure client

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ export { CustomUtils } from './custom-utils';
 export { CustomResult } from './custom-result';
 export { CustomFirebase } from './custom-firebase';
 export { CustomValidator } from './custom-validator';
+export { CustomMongoClient } from './custom-mongo-client';
+export { CustomAzureStorage } from './custom-azure-storage';
+
 export * as CustomDefinition from './custom-definition';
