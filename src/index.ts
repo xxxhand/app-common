01df1224ae6667d520e054cc281ecaf826f5c185
xxxhand <nohandboy@gmail.com>
2024-05-22 13:53:41 +0800

    Add abstract mongoose client

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ export { CustomFirebase } from './custom-firebase';
 export { CustomValidator } from './custom-validator';
 export { CustomMongoClient } from './custom-mongo-client';
 export { CustomAzureStorage } from './custom-azure-storage';
+export { TMongooseClient } from './abstract-mongoose-client';
 
 export * as CustomDefinition from './custom-definition';
