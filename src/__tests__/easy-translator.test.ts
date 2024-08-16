5c12fcef503f49f1dc2e00dd83394ffc17a075a2
xxxhand <nohandboy@gmail.com>
2024-08-16 14:47:03 +0800

    Add abstract easy translator

---
+++ src/__tests__/easy-translator.test.ts
@@ -0,0 +1,26 @@
+import { TEasyTranslator } from '../abstract-easy-translator';
+
+describe('Easy translator test', () => {
+  let trs: TEasyTranslator;
+  beforeAll(async () => {
+    class MyTranslator extends TEasyTranslator { }
+    trs = await new MyTranslator()
+      .useDictionary('dev', { 'HELLO': 'World' })
+      .useDictionary('en', { 'HELLO': 'World!!' })
+      .useDictionary('zh-tw', { 'HELLO': '你好' })
+      .useFallbackLng('dev')
+      .initial();
+  })
+  test('Should translate to en', () => {
+    expect(trs.t('HELLO', 'en')).toBe('World!!');
+  });
+  test('Should translate to zh-tw', () => {
+    expect(trs.t('HELLO', 'zh-TW')).toBe('你好');
+  });
+  test('Should translate to fallback lang', () => {
+    expect(trs.t('HELLO')).toBe('World');
+  });
+  test('Not found key, should be input key', () => {
+    expect(trs.t('HELLOLO')).toBe('HELLOLO');
+  });
+});
---
+++ src/abstract-easy-translator.ts
@@ -0,0 +1,77 @@
+import * as fs from 'fs-extra';
+import { CustomValidator } from './custom-validator';
+
+export abstract class TEasyTranslator {
+  /** Prefix for console log */
+  protected errPrefix: string = '';
+
+  /** Files extension that being supported, default to json */
+  protected supportExtenstion: string = '.json';
+
+  /** Directory path that includes [locale].json */
+  protected resourcesPath: string = '';
+
+  /** Default locale if not found */
+  protected fallbackLng: string = 'dev';
+
+  /** Dictionary, this content would like {'en', {'IS_EMPTY': 'is empty!!' } } */
+  protected translationDictionary: Map<string, Record<string, string>> = new Map();
+
+  /** Use the directory path that includes [locale].json, it must be absoulte path */
+  public useResources(absoultePath: string): TEasyTranslator {
+    if (CustomValidator.nonEmptyString(absoultePath)) {
+      this.resourcesPath = absoultePath;
+    }
+    return this;
+  }
+
+  /** Use specified dictionary w/ locale */
+  public useDictionary(locale: string, dict: Record<string, string>): TEasyTranslator {
+    this.translationDictionary.set(locale.toLowerCase(), dict);
+    return this;
+  }
+
+  /** Use default locale if not found */
+  public useFallbackLng(lng: string): TEasyTranslator {
+    if (CustomValidator.nonEmptyString(lng)) {
+      this.fallbackLng = lng.toLowerCase();
+    }
+    return this;
+  }
+
+  public async initial(): Promise<TEasyTranslator> {
+    if (CustomValidator.nonEmptyString(this.resourcesPath)) {
+      const exists = await fs.exists(this.resourcesPath);
+      if (!exists) {
+        throw new Error(`${this.errPrefix} The reources directory is not exists`);
+      }
+      const stat = await fs.stat(this.resourcesPath);
+      if (!stat.isDirectory()) {
+        throw new Error(`${this.errPrefix} Input path ${this.resourcesPath} is not a directory`);
+      }
+      const files = await fs.readdir(this.resourcesPath);
+      const acceptedFiles = files.filter((x) => x.endsWith(this.supportExtenstion));
+      if (!CustomValidator.nonEmptyArray(acceptedFiles)) {
+        throw new Error(`${this.errPrefix} Input path ${this.resourcesPath} must have at least one file named ${this.fallbackLng}${this.supportExtenstion}`);
+      }
+      for (let i = 0; i < acceptedFiles.length; i++) {
+        const lng = files[i].substring(0, files[i].lastIndexOf(this.supportExtenstion));
+        console.log(`${this.errPrefix} Load support lang: ${lng}`);
+        const content = await fs.readFile(`${this.resourcesPath}/${files[i]}`, { encoding: 'utf-8' });
+        this.translationDictionary.set(lng.toLowerCase(), JSON.parse(content));
+      }
+    }
+    return this;
+  }
+
+  public t(key: string, locale?: string): string {
+    if (!(CustomValidator.nonEmptyString(locale) && this.translationDictionary.has(locale!.toLowerCase()))) {
+      locale = this.fallbackLng;
+    }
+    const str = (this.translationDictionary.get(locale!.toLowerCase()) as Record<string, string>)[key];
+    if (CustomValidator.nonEmptyString(str)) {
+      return str;
+    }
+    return key;
+  }
+}
