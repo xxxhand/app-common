99e958c4970ab2c40462499bf3c927f0c037b50b
Wilbert Yang <Wilbert_Yang@compal.com>
2025-05-28 13:31:26 +0800

    refactor: add functions with container name for cusom-azure-storage

--- src/custom-azure-storage.ts
+++ src/custom-azure-storage.ts
@@ -23,6 +23,11 @@ export class CustomAzureStorage {
   // blob service client
   private _blobServiceClient: BlobServiceClient | null = null;
 
+  get blobServiceClient(): BlobServiceClient | null {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+    return this._blobServiceClient;
+  }
+
   private readonly _error_prefix = '[custom-azure-storage]';
 
   private _accountName: string = '';
@@ -37,6 +42,7 @@ export class CustomAzureStorage {
     return this;
   }
 
+  // Will be deprecated in future versions
   // Set container name
   public useContainerName(containerName: string): CustomAzureStorage {
     this._containerName = containerName;
@@ -70,6 +76,7 @@ export class CustomAzureStorage {
     return this._blobServiceClient.getContainerClient(this._containerName);
   }
 
+  // Will be deprecated in future versions
   // Upload file to azure storage
   public async uploadFile(source: Buffer | string, targetFile: string): Promise<void> {
     if (!this._blobServiceClient) {
@@ -87,6 +94,21 @@ export class CustomAzureStorage {
     }
   }
 
+  // Upload file to azure storage
+  public async upload(source: Buffer | string, container: string, path: string): Promise<void> {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+
+    const containerClient = this._blobServiceClient.getContainerClient(container);
+    if (!await containerClient.exists()) await containerClient.create();
+    const blobClient = containerClient.getBlockBlobClient(path);
+    if (typeof source === 'string') {
+      await blobClient.uploadFile(source);
+    } else {
+      await blobClient.upload(source, source.length);
+    }
+  }
+
+  // Will be deprecated in future versions
   // Download file from azure storage
   public async downloadFile(fileName: string): Promise<BlobDownloadResponseParsed> {
     if (!this._blobServiceClient) {
@@ -98,6 +120,18 @@ export class CustomAzureStorage {
     return blobClient.download();
   }
 
+  // Download file from azure storage
+  public async download(container: string, fileName: string): Promise<BlobDownloadResponseParsed> {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+
+    const blobClient = this._blobServiceClient
+      .getContainerClient(container)
+      .getBlockBlobClient(fileName);
+    if (!await blobClient.exists()) throw new Error(`${this._error_prefix} The file is not exist`);
+    return blobClient.download();
+  }
+
+  // Will be deprecated in future versions
   // Delete the file from azure storage
   public async deleteFile(fileName: string): Promise<void> {
     if (!this._blobServiceClient) {
@@ -108,6 +142,17 @@ export class CustomAzureStorage {
     await blobClient.delete();
   }
 
+  // Delete the file from azure storage
+  public async delete(container: string, fileName: string): Promise<void> {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+
+    await this._blobServiceClient
+      .getContainerClient(container)
+      .getBlockBlobClient(fileName)
+      .delete();
+  }
+
+  // Will be deprecated in future versions
   // Copy the file from azure storage
   public async copyFile(sourceFileName: string, targetFileName: string): Promise<void> {
     if (!this._blobServiceClient) {
@@ -120,6 +165,18 @@ export class CustomAzureStorage {
     await copyPoller.pollUntilDone();
   }
 
+  // Copy the file from azure storage
+  public async copy(container: string, sourceFileName: string, targetFileName: string): Promise<void> {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+
+    const containerClient = this._blobServiceClient.getContainerClient(container);
+    const sourceBlobClient = containerClient.getBlockBlobClient(sourceFileName);
+    const targetBlobClient = containerClient.getBlockBlobClient(targetFileName);
+    const copyPoller = await targetBlobClient.beginCopyFromURL(sourceBlobClient.url);
+    await copyPoller.pollUntilDone();
+  }
+
+  // Will be deprecated in future versions
   // Create blob SAS token
   public async createBlobSasToken(fileName: string, duration: number, permissions: string = 'r'): Promise<string> {
     if (!this._blobServiceClient) {
@@ -136,9 +193,24 @@ export class CustomAzureStorage {
     return sasToken;
   }
 
-  // Create account storage folder SAS token
-  public generateAzureStorageSASToken(
-    containerName: string,
+  // Create blob SAS token
+  public async createSasToken(container: string, fileName: string, duration: number, permissions: string = 'r'): Promise<string> {
+    if (!this._blobServiceClient) throw new Error(`${this._error_prefix} The blob service client is not initial`);
+
+    const blobClient = this._blobServiceClient
+      .getContainerClient(container)
+      .getBlockBlobClient(fileName);
+    return blobClient.generateSasUrl({
+      permissions: BlobSASPermissions.parse(permissions),
+      protocol: SASProtocol.Https,
+      startsOn: new Date(),
+      expiresOn: new Date(new Date().getTime() + duration),
+    });
+  }
+
+  // Create account storage file/folder SAS token
+  public createFolderSASToken(
+    container: string,
     pathName: string,
     isDirectory: boolean = false,
     duration: number = 60 * 60 * 24,
@@ -147,7 +219,7 @@ export class CustomAzureStorage {
     if (!this._storageSharedKeyCredential) throw new Error(`${this._error_prefix} The storage shared key credential is not initial`);
     return generateDataLakeSASQueryParameters(
       {
-        fileSystemName: containerName,
+        fileSystemName: container,
         startsOn: new Date(),
         expiresOn: new Date(new Date().getTime() + duration * 1000),
         permissions: DataLakeSASPermissions.parse(permissions),
